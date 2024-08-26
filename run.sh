3f836cd8a85a9ad6a80ee0fa37df9861cbb079c1
chickenj0 <github.skimmer925@passmail.net>
2024-08-26 19:32:38 +0800

    Update run.sh
    Signed-off-by: chickenj0 <github.skimmer925@passmail.net>

--- run.sh
+++ run.sh
@@ -1,46 +1,26 @@
 #!/bin/bash
-# run.sh
 
-# If no argument is provided, display the usage message
+# Check if the script argument is provided
 if [ -z "$1" ]; then
-    echo "Usage: sudo run <script_path>"
-    echo ""
-    echo "To see what else you can do with 'run', simply type:"
-    echo "sudo run"
-    echo ""
-    echo "This command allows you to:"
-    echo "- Execute any script by providing its path."
-    echo "- The script will be made executable and then run."
-    echo ""
-    echo "You can also list available scripts in the scripts/ directory:"
-    echo "sudo run list"
-    exit 1
+  echo "Error: No script name provided."
+  echo "Usage: run <script.sh>"
+  exit 1
 fi
 
-# Handle the 'list' argument to list all scripts in the scripts/ directory
-if [ "$1" == "list" ]; then
-    echo "Run any of the scripts below by running: sudo run <example.sh>"
-    for script in ./scripts/*.sh; do
-        basename "$script"
-    done
-    exit 0
-fi
-
-script_name="$1"
-
-# Prepend the 'scripts/' directory if the script_name doesn't contain a path
-if [[ "$script_name" != */* ]]; then
-    script_path="./scripts/$script_name"
+# Search in the current directory, /usr/local/bin/fabric, and by absolute path
+if [ -f "$1" ]; then
+  script_path="./$1"
+elif [ -f "/usr/local/bin/fabric/$1" ]; then
+  script_path="/usr/local/bin/fabric/$1"
+elif [ -f "$(realpath "$1")" ]; then
+  script_path=$(realpath "$1")
 else
-    script_path="$script_name"
+  echo "Error: Script '$1' not found."
+  exit 1
 fi
 
-# Check if the script exists
-if [ ! -f "$script_path" ]; then
-    echo "Error: Script '$script_name' not found."
-    exit 1
-fi
-
-# Make the script executable and run it
+# Make the script executable
 chmod +x "$script_path"
+
+# Run the script
 "$script_path"
