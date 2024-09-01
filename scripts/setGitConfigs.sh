83c0017a87323461518c97611ca82c8f4b0ff79b
Henry <henry@codemonkey.net.au>
2024-09-01 21:16:39 +0800

    setGitConfigs.sh

---
+++ scripts/setGitConfigs.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+read -p "What is the email you want to use for this git repo?: " EMAIL
+git config --global user.email $EMAIL
+
+read -p "What is your name?: " NAME
+git config --global user.name $NAME
+
+echo "done"
