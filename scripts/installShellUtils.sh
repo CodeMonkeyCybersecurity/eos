d73a76af35bdf3bf2c30043231feab3016d1374e
Henry <henry@codemonkey.net.au>
2024-09-01 21:19:58 +0800

    modified:   installShellUtils.sh

--- scripts/installShellUtils.sh
+++ scripts/installShellUtils.sh
@@ -1,4 +1,8 @@
-sudo apt install gh tree ncdu zfs* powershell prometheus zx git 
+#!/bin/bash
+
+sudo apt install gh tree ncdu zfs* prometheus zx git 
+
+sudo snap install powershell
 
 gh auth login 
 
