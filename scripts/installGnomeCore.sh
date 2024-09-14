1f4b7e26338599238c720f341ecf3d293c6d20aa
chickenj0 <github.skimmer925@passmail.net>
2024-09-14 15:16:29 +0800

    Update installGnomeCore.sh
    Signed-off-by: chickenj0 <github.skimmer925@passmail.net>

--- scripts/installGnomeCore.sh
+++ scripts/installGnomeCore.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
 echo "updating apt"
-sudo apt update && sudo apt upgrade -y
-echo "updated apt"
+sudo apt update && sudo apt dist-upgrade -y && sudo apt autoremove -y && sudo apt autoclean
+echo "updated and upgraded apt"
 
-echo "installing gnome-core"
 sudo apt install gnome-core -y
 echo "installed gnome-core"
 
-echo "done"
+sudo apt install gnome-remote-desktop -y
+echo "installed gnome-remote-desktop"
+
+sudo ufw allow 3389/tcp  # For RDP
+sudo ufw enable && sudo ufw reload
+echo "firewall configs adapted"
+
+sudo systemctl enable gnome-remote-desktop.service
+echo "gnome-remote-desktop.service"
+
+echo "finis"
