690f9ee6882572c12326bf8217abe71f279ab633
Chickenj0 <github.skimmer925@passmail.net>
2024-09-29 08:05:48 +0800

    Update setupUbuntu.sh
    Signed-off-by: Chickenj0 <github.skimmer925@passmail.net>

--- scripts/setupTools.sh
+++ scripts/setupTools.sh
@@ -5,8 +5,9 @@ sudo systemctl daemon-reload \
 
 sudo apt install -y \
 nfs-kernel-server nfs-common \
+mailutils lm-sensors \
 gh tree ncdu ceph ceph-deploy btrfs-progs \
-hub \
+openssh \ 
 nmap \
 htop iftop iotop nload glances\
 prometheus \
@@ -17,6 +18,7 @@ python3-fabric python3-pip\
 ansible \
 nginx \
 borgbackup \
+ufw \ 
 
 npm && \
 
@@ -27,3 +29,10 @@ sudo snap install powershell  --classic && \
 sudo snap refresh
 
 sudo apt update && sudo systemctl daemon-reload
+sudo ufw allow openssh 
+sudo ufw allow http
+sudo ufw allow https
+sudo systemctl enable ufw && sudo systemctl start ufw
+sudo apt update && sudo apt dist-upgrade -y && sudo apt autoremove && sudo apt autoclean \
+
+
