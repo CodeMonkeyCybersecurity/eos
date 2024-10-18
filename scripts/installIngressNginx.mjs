3e534aa956a9ad2d3743acda6bb05b2c2f2e4dce
Code Monkey Cybersecurity <henry@cybermonkey.net.au>
2024-10-18 08:17:10 +0800

    Update installIngressNginx.mjs
    Signed-off-by: Code Monkey Cybersecurity <henry@cybermonkey.net.au>

--- scripts/installIngressNginx.mjs
+++ scripts/installIngressNginx.mjs
@@ -1,11 +1,16 @@
 #!/usr/bin/env zx
 
-// Define the necessary Helm command and arguments
-const helmCommand = [
-  'helm', 'upgrade', '--install', 'ingress-nginx', 'ingress-nginx',
-  '--repo', 'https://kubernetes.github.io/ingress-nginx',
-  '--namespace', 'ingress-nginx', '--create-namespace'
-];
+async function installHelm() {
+    console.log('Adding ingress-nginx using Helm...');
+    await $`helm upgrade --install ingress-nginx ingress-nginx --repo https://kubernetes.github.io/ingress-nginx  --namespace ingress-nginx --create-namespace`;
+    console.log('Ingress-nginx installed successfully.');
+}
 
-// Execute the Helm command
-await $`${helmCommand}`;
+installHelm().catch(err => {
+    console.error('Error during installation:', err);
+});
+
+
+
+ \
+ 
