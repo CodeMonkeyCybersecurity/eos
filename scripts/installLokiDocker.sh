f0030ca505cd863c97be7142047468789053e7cd
Code Monkey Cybersecurity <henry@cybermonkey.net.au>
2024-10-23 18:17:37 +0800

    Create installLokiDocker.sh
    Signed-off-by: Code Monkey Cybersecurity <henry@cybermonkey.net.au>

---
+++ scripts/installLokiDocker.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+mkdir loki && cd loki \
+wget https://raw.githubusercontent.com/grafana/loki/v3.0.0/production/docker-compose.yaml -O docker-compose.yaml && \
+docker-compose -f docker-compose.yaml up -d \
+echo "finis"
