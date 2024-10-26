f13cceebe082b67c0ca8a2e236a1a1f6de16ac83
Code Monkey Cybersecurity <git@cybermonkey.net.au>
2024-10-26 09:45:29 +0800

    Update installZx.sh
    Signed-off-by: Code Monkey Cybersecurity <git@cybermonkey.net.au>

--- scripts/installZx.sh
+++ scripts/installZx.sh
@@ -1,15 +1,6 @@
-#!/usr/bin/env zx
+#!/bin/bash
 
-await $`cat package.json | grep name`
-
-let branch = await $`git branch --show-current`
-await $`dep deploy --branch=${branch}`
-
-await Promise.all([
-  $`sleep 1; echo 1`,
-  $`sleep 2; echo 2`,
-  $`sleep 3; echo 3`,
-])
-
-let name = 'foo bar'
-await $`mkdir /tmp/${name}`
+sudo apt update 
+sudo apt install npm 
+sudo npm install -g zx
+echo "finis"
