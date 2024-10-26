07d8b9e190d19e19a0f1288db96f64db4344c80f
Code Monkey Cybersecurity <git@cybermonkey.net.au>
2024-10-26 09:35:06 +0800

    Create backupConfFile.mjs
    Signed-off-by: Code Monkey Cybersecurity <git@cybermonkey.net.au>

---
+++ scripts/backupConfFile.mjs
@@ -0,0 +1,21 @@
+// backupConfFile.mjs
+import { $ } from 'zx'
+import path from 'path'
+
+if (process.argv.length < 3) {
+  console.error("Usage: zx backupConfFile.mjs <file_to_backup>")
+  process.exit(1)
+}
+
+const fileToBackup = process.argv[2]
+const dir = path.dirname(fileToBackup)
+const filename = path.basename(fileToBackup)
+const date = new Date().toISOString().split('T')[0]  // YYYY-MM-DD format
+const time = new Date().toLocaleTimeString('en-GB', { hour12: false }).replace(/:/g, '')  // HHMMSS format
+
+const backupFile = `${dir}/${filename}.backup.${date}${time}`
+
+// Copy the file to a backup location
+await $`cp ${fileToBackup} ${backupFile}`
+console.log(`Backup created: ${backupFile}`)
+
