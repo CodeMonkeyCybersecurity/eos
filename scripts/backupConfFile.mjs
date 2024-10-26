7024550acb0f74201422b8e0a1dfadbacb4acf36
Code Monkey Cybersecurity <git@cybermonkey.net.au>
2024-10-26 10:03:28 +0800

    Update backupConfFile.mjs
    Signed-off-by: Code Monkey Cybersecurity <git@cybermonkey.net.au>

--- scripts/backupConfFile.mjs
+++ scripts/backupConfFile.mjs
@@ -1,22 +1,15 @@
 #!/usr/bin/env zx
 
-import { question } from 'zx'
 import { $ } from 'zx'
 import path from 'path'
 
-// Check if <file_to_backup> is specified
-let fileToBackup = process.argv[2]
-
-if (!fileToBackup) {
-  console.error("No file specified for backup.")
-  // Prompt the user to enter a file path if not provided as an argument
-  fileToBackup = await question("Please enter the path to the file you want to back up: ")
-  if (!fileToBackup) {
-    console.error("No file specified. Exiting.")
-    process.exit(1)
-  }
+// Check if <file_to_backup> is specified as an argument
+if (process.argv.length < 3) {
+  console.error("Usage: zx backupConfFile.mjs <file_to_backup>")
+  process.exit(1)
 }
 
+const fileToBackup = process.argv[2]
 const dir = path.dirname(fileToBackup)
 const filename = path.basename(fileToBackup)
 const date = new Date().toISOString().split('T')[0]  // YYYY-MM-DD format
