aea6c190a5423c4d7cc34f60e0269f421bb318ed
Henry <git@cybermonkey.net.au>
2024-10-26 03:24:35 +0000

    modified:   setupTools.sh

--- scripts/setupTools.sh
+++ scripts/setupTools.sh
@@ -6,21 +6,10 @@ sudo systemctl daemon-reload \
 sudo apt install -y \
 nfs-kernel-server nfs-common \
 mailutils lm-sensors \
-gh tree ncdu ceph ceph-deploy btrfs-progs \
-openssh \ 
-nmap \
-htop iftop iotop nload glances\
-prometheus \
-zx \
-git \
-fzf \
-python3-fabric python3-pip\
-ansible \
-nginx \
-borgbackup \
-ufw \ 
-
-npm && \
+gh tree ncdu ssh nmap wireguard \
+htop iftop iotop nload glances \
+prometheus zx git fzf python3-pip \
+nginx borgbackup etckeeper ufw npm && \
 
 npm install -g zx && \
 
@@ -29,7 +18,7 @@ sudo snap install powershell  --classic && \
 sudo snap refresh
 
 sudo apt update && sudo systemctl daemon-reload
-sudo ufw allow openssh 
+sudo ufw allow openssh
 sudo ufw allow http
 sudo ufw allow https
 sudo systemctl enable ufw && sudo systemctl start ufw
