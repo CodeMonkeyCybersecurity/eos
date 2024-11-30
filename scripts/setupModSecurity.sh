83af18ac9044d10349c974c4e52d95157ab32cd1
Code Monkey Cybersecurity <git@cybermonkey.net.au>
2024-11-30 22:38:53 +0800

    Update setupModSecurity.sh
    Signed-off-by: Code Monkey Cybersecurity <git@cybermonkey.net.au>

--- scripts/setupModSecurity.sh
+++ scripts/setupModSecurity.sh
@@ -13,74 +13,79 @@ command_exists() {
     command -v "$1" >/dev/null 2>&1
 }
 
-apt install nginx || error_exit "Failed to install nginx."
+install_nginx() {
+    apt install nginx || error_exit "Failed to install nginx."
+    nginx -V || error_exit "Failed to verify nginx version."
+    apt install software-properties-common || error_exit "sudo apt install software-properties-common  failed"
+    apt-add-repository -ss || error_exit "sudo apt-add-repository -ss failed"
+    apt update || error_exit "sudo apt update failed"
 
-nginx -V || error_exit "Failed to verify nginx version."
+download_source() {
+    echo "get username..." 
+    read -p "Which user would you like to administer nginx?: " INPUT_USER || error_exit "read -p "Which user would you like to administer nginx?: " INPUT_USER failed"
+    chown "$INPUT_USER:$INPUT_USER" /usr/local/src/ -R || error_exit "chown "$INPUT_USER:$INPUT_USER" /usr/local/src/ -R failed"
+    mkdir -p /usr/local/src/nginx || exit_error "failed to mkdir: /usr/local/src/nginx"
+    cd /usr/local/src/nginx/ || exit_error "failed to cd into the Nginx source directory."
+    echo "Download Nginx source package: "
+    apt install dpkg-dev 
+    apt source nginx 
+    echo "list the downloaded source files..."
+    echo "note the version of nginx which has been installed. You will need to input it in the next step..."
+    ls -lah /usr/local/src/nginx/
+    read -p "Enter nginx version number (eg. 1.24.0): " VERSION_NUMBER
+}
 
-apt install software-properties-common || error_exit "sudo apt install software-properties-common  failed"
-apt-add-repository -ss || error_exit "sudo apt-add-repository -ss failed"
-apt update || error_exit "sudo apt update failed"
+install_libmodsecurity() {
+    echo "To compile libmodsecurity, first clone the source code from Github..."
+    apt install git || error_exit "Failed to install git"
+    git clone --depth 1 -b v3/master --single-branch https://github.com/SpiderLabs/ModSecurity /usr/local/src/ModSecurity/ || error_exit "Failed to clone ModSecurity."
+    cd /usr/local/src/ModSecurity/ || error_exit "Failed to navigate to ModSecurity directory."
+    echo "Install build dependencies..."
+    apt install gcc make build-essential autoconf \
+    automake libtool libcurl4-openssl-dev liblua5.3-dev libpcre2-dev \
+    libfuzzy-dev ssdeep gettext pkg-config libpcre3 libpcre3-dev \
+    libxml2 libxml2-dev libcurl4 libgeoip-dev libyajl-dev doxygen \
+    uuid-dev || error_exit "Failed to install all dependencies."
+    echo "Install required submodules..."
+    git submodule init || error_exit "Failed to initialize submodules."
+    git submodule update || error_exit "Failed to update submodules."
+    echo "Configure the build environment..."
+    ./build.sh || error_exit "./build.sh failed"
+    ./configure || error_exit "./configure failed"
+    make || error_exit "make failed"
+    echo "After the make command finished without errors, install the binary..."
+    make install || error_exit "make install failed"
 
-echo "get username..." 
-read -p "Which user would you like to administer nginx?: " INPUT_USER || error_exit "read -p "Which user would you like to administer nginx?: " INPUT_USER failed"
-chown "$INPUT_USER:$INPUT_USER" /usr/local/src/ -R || error_exit "chown "$INPUT_USER:$INPUT_USER" /usr/local/src/ -R failed"
 
-mkdir -p /usr/local/src/nginx
-cd /usr/local/src/nginx/
+# Compile ModSecurity Nginx connector
+compile_nginx_connector() {
+    echo "[Info] Compiling ModSecurity Nginx connector..."
+    echo "Download and Compile ModSecurity v3 Nginx Connector Source Code..."
+    git clone --depth 1 https://github.com/SpiderLabs/ModSecurity-nginx.git /usr/local/src/ModSecurity-nginx/ || error_exit "Failed to clone ModSecurity Nginx connector."
+    cd /usr/local/src/nginx/nginx-$VERSION_NUMBER/ || error_exit "Failed to navigate to Nginx source directory."
+    apt build-dep nginx || error_exit "Install build dependencies for Nginx failed."
+    ./configure --with-compat --add-dynamic-module=/usr/local/src/ModSecurity-nginx || error_exit "Failed to configure Nginx for ModSecurity."
+    make modules || error_exit "Failed to build ModSecurity Nginx module."
+    cp objs/ngx_http_modsecurity_module.so /usr/share/nginx/modules/ || error_exit "Failed to copy ModSecurity module."
+}
 
-echo "Download Nginx source package:"
-apt install dpkg-dev 
-apt source nginx 
 
-echo "list the downloaded source files..."
-ls
+# Configure Nginx for ModSecurity
+load_connector_module() {
+    echo "[Info] Configuring Nginx..."
+    echo "Load the ModSecurity v3 Nginx Connector Module..."
+    #sudo mkdir -p /etc/nginx/modsec || error_exit "Failed to create ModSecurity configuration directory."
+    sudo cp /usr/local/src/ModSecurity/modsecurity.conf-recommended /etc/nginx/modsec/modsecurity.conf || error_exit "Failed to copy ModSecurity configuration."
+    sudo cp /usr/local/src/ModSecurity/unicode.mapping /etc/nginx/modsec/ || error_exit "Failed to copy unicode mapping file."
+    sudo sed -i 's/SecRuleEngine DetectionOnly/SecRuleEngine On/' /etc/nginx/modsec/modsecurity.conf || error_exit "Failed to enable SecRuleEngine."
+    echo "Include /etc/nginx/modsec/modsecurity.conf;" | sudo tee -a /etc/nginx/modsec/main.conf >/dev/null
+    echo "Include /etc/nginx/modsec/coreruleset-3.3.4/crs-setup.conf;" | sudo tee -a /etc/nginx/modsec/main.conf >/dev/null
+    echo "Include /etc/nginx/modsec/coreruleset-3.3.4/rules/*.conf;" | sudo tee -a /etc/nginx/modsec/main.conf >/dev/null
+    echo "load_module modules/ngx_http_modsecurity_module.so;" | sudo tee -a /etc/nginx/nginx.conf >/dev/null
+    sudo nginx -t || error_exit "Nginx configuration test failed."
+    sudo systemctl restart nginx || error_exit "Failed to restart Nginx."
+}
 
-echo "To compile libmodsecurity, first clone the source code from Github..."
-apt install git || error_exit "Failed to install git"
-git clone --depth 1 -b v3/master --single-branch https://github.com/SpiderLabs/ModSecurity /usr/local/src/ModSecurity/ || error_exit "Failed to clone ModSecurity."
-cd /usr/local/src/ModSecurity/ || error_exit "Failed to navigate to ModSecurity directory."
-
-echo "Install build dependencies..."
-apt install gcc make build-essential autoconf \
-automake libtool libcurl4-openssl-dev liblua5.3-dev libpcre2-dev \
-libfuzzy-dev ssdeep gettext pkg-config libpcre3 libpcre3-dev \
-libxml2 libxml2-dev libcurl4 libgeoip-dev libyajl-dev doxygen 
-
-echo "Install required submodules..."
-git submodule init || error_exit "Failed to initialize submodules."
-git submodule update || error_exit "Failed to update submodules."
-
-echo "Configure the build environment..."
-./build.sh 
-./configure
-make 
-
-echo "After the make command finished without errors, install the binary..."
-make install
-
-echo "Download and Compile ModSecurity v3 Nginx Connector Source Code..."
-git clone --depth 1 https://github.com/SpiderLabs/ModSecurity-nginx.git /usr/local/src/ModSecurity-nginx/
-
-echo "note the version of nginx which has been installed. You will need to input it in the next step..."
-ls -lah /usr/local/src/nginx/
-
-read -p "Enter nginx version number (eg. 1.24.0): " VERSION_NUMBER
-cd /usr/local/src/nginx/nginx-$VERSION_NUMBER/
-
-echo "Install build dependencies for Nginx..."
-apt build-dep nginx
-apt install uuid-dev
-
-echo "configure the environment ..."
-./configure --with-compat --with-openssl=/usr/include/openssl/ --add-dynamic-module=/usr/local/src/ModSecurity-nginx
-
-echo "Build the ModSecurity Nginx Connector module..."
-make modules
-
-echo "Copy 'objs/ngx_http_modsecurity_module.so' it to the '/usr/share/nginx/modules/' directory."
-cp objs/ngx_http_modsecurity_module.so /usr/share/nginx/modules/
-
-echo "Load the ModSecurity v3 Nginx Connector Module..."
 echo "Edit the main Nginx configuration file..."
 # todo Add the following line at the beginning of the file.
 
@@ -130,31 +135,9 @@ systemctl restart nginx
 
 
 
-# Compile ModSecurity Nginx connector
-compile_nginx_connector() {
-    mkdir -p /usr/local/src/nginx/
-    echo "[Info] Compiling ModSecurity Nginx connector..."
-    sudo git clone --depth 1 https://github.com/SpiderLabs/ModSecurity-nginx.git /usr/local/src/ModSecurity-nginx/ || error_exit "Failed to clone ModSecurity Nginx connector."
-    cd /usr/local/src/nginx/ || error_exit "Failed to navigate to Nginx source directory."
-    sudo ./configure --with-compat --add-dynamic-module=/usr/local/src/ModSecurity-nginx || error_exit "Failed to configure Nginx for ModSecurity."
-    sudo make modules || error_exit "Failed to build ModSecurity Nginx module."
-    sudo cp objs/ngx_http_modsecurity_module.so /usr/share/nginx/modules/ || error_exit "Failed to copy ModSecurity module."
-}
 
-# Configure Nginx for ModSecurity
-configure_nginx() {
-    echo "[Info] Configuring Nginx..."
-    sudo mkdir -p /etc/nginx/modsec || error_exit "Failed to create ModSecurity configuration directory."
-    sudo cp /usr/local/src/ModSecurity/modsecurity.conf-recommended /etc/nginx/modsec/modsecurity.conf || error_exit "Failed to copy ModSecurity configuration."
-    sudo cp /usr/local/src/ModSecurity/unicode.mapping /etc/nginx/modsec/ || error_exit "Failed to copy unicode mapping file."
-    sudo sed -i 's/SecRuleEngine DetectionOnly/SecRuleEngine On/' /etc/nginx/modsec/modsecurity.conf || error_exit "Failed to enable SecRuleEngine."
-    echo "Include /etc/nginx/modsec/modsecurity.conf;" | sudo tee -a /etc/nginx/modsec/main.conf >/dev/null
-    echo "Include /etc/nginx/modsec/coreruleset-3.3.4/crs-setup.conf;" | sudo tee -a /etc/nginx/modsec/main.conf >/dev/null
-    echo "Include /etc/nginx/modsec/coreruleset-3.3.4/rules/*.conf;" | sudo tee -a /etc/nginx/modsec/main.conf >/dev/null
-    echo "load_module modules/ngx_http_modsecurity_module.so;" | sudo tee -a /etc/nginx/nginx.conf >/dev/null
-    sudo nginx -t || error_exit "Nginx configuration test failed."
-    sudo systemctl restart nginx || error_exit "Failed to restart Nginx."
-}
+
+
 
 # Download and enable OWASP CRS
 setup_owasp_crs() {
@@ -167,12 +150,11 @@ setup_owasp_crs() {
 
 # Main function
 main() {
-    install_dependencies
     install_nginx
-    check_nginx_version
-    setup_modsecurity
+    download_source
+    install_libmodsecurity
     compile_nginx_connector
-    configure_nginx
+    load_connector_module
     setup_owasp_crs
     echo "[Success] ModSecurity with Nginx has been successfully set up."
 }
