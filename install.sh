2164a2ebecd1ef3330ee8989b1df353c76edd9ca
Code Monkey Cybersecurity <git@cybermonkey.net.au>
2024-12-01 10:47:54 +0800

    Update install.sh
    Signed-off-by: Code Monkey Cybersecurity <git@cybermonkey.net.au>

--- install.sh
+++ install.sh
@@ -1,5 +1,33 @@
 #!/bin/bash
+# TODO : move to usr/bin/env to create CLI variable  
 
+logError() {
+    local errorMessage="$1"
+    local logDir="./logs"
+    local logFile="$logDir/error.log"
+
+    # Create the logs directory if it doesn't exist
+    if [ ! -d "$logDir" ]; then
+        mkdir -p "$logDir"
+    fi
+
+    # Get the current timestamp
+    local timestamp
+    timestamp=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
+
+    # Format the log entry
+    local logEntry
+    logEntry="[$timestamp] ERROR: $errorMessage"
+
+    # Append the error message to the log file
+    echo "$logEntry" >> "$logFile"
+
+    # Output the error to the console
+    echo "An error occurred: $errorMessage" >&2
+}
+
+# Example usage
+logError "This is a sample error message"
 # Get the original directory (where the script is run from)
 ORIGINAL_DIR=$(pwd)
 
