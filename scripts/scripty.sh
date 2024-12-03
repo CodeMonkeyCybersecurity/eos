01ac32064c7292e62b09c0fd57b67da5791c84fb
Code Monkey Cybersecurity <git@cybermonkey.net.au>
2024-12-03 20:48:42 +0800

    Update scripty.sh
    Signed-off-by: Code Monkey Cybersecurity <git@cybermonkey.net.au>

--- scripts/scripty.sh
+++ scripts/scripty.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
 # Create a directory for log files if it doesn't exist
-LOG_DIR="mnemosyne_logs"
-mkdir -p "$LOG_DIR"
+LOG_DIR="$HOME"
 
 # Initialize variables
 LINE_LIMIT=1000
 FILE_COUNT=1
-CURRENT_FILE="$LOG_DIR/$(date +"%Y-%m-%d_%H-%M")_log_$FILE_COUNT.txt"
+CURRENT_FILE="$LOG_DIR/$(date +"%Y-%m-%d_%H-%M")_mnemosyne_$FILE_COUNT.txt"
 
 # Function to check and rotate log files
 rotate_log_file() {
     if [[ $(wc -l < "$CURRENT_FILE") -ge $LINE_LIMIT ]]; then
         ((FILE_COUNT++))
-        CURRENT_FILE="$LOG_DIR/$(date +"%Y-%m-%d_%H-%M")_log_$FILE_COUNT.txt"
+        CURRENT_FILE="$LOG_DIR/$(date +"%Y-%m-%d_%H-%M")_mnemosyne_$FILE_COUNT.txt"
         echo "Switched to new log file: $CURRENT_FILE"
     fi
 }
 
 # Start the script command with logging to the initial file
 echo "Starting logging to $CURRENT_FILE"
-script -q -c "/bin/zsh" "$CURRENT_FILE" | while read -r line; do
+script -q /dev/null | tee -a "$CURRENT_FILE" | while IFS= read -r line; do
+    echo "$line" | tee -a "$CURRENT_FILE" > /dev/null
     rotate_log_file
 done
