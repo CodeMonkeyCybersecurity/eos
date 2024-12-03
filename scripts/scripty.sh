8a47e9c584f8f5c1edf39af9cf7d17f778c437b6
Code Monkey Cybersecurity <git@cybermonkey.net.au>
2024-12-03 20:16:50 +0800

    Update scripty.sh
    Signed-off-by: Code Monkey Cybersecurity <git@cybermonkey.net.au>

--- scripts/scripty.sh
+++ scripts/scripty.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
 
-echo "running: alias scripty='script "$(date +"%Y-%m-%d %H:%M") mnemosyne.txt"'"
-alias scripty='script "$(date +"%Y-%m-%d %H:%M") mnemosyne.txt"'
+# Create a directory for log files if it doesn't exist
+LOG_DIR="mnemosyne_logs"
+mkdir -p "$LOG_DIR"
 
-echo "finis"
+# Initialize variables
+LINE_LIMIT=1000
+FILE_COUNT=1
+CURRENT_FILE="$LOG_DIR/$(date +"%Y-%m-%d_%H-%M")_log_$FILE_COUNT.txt"
+
+# Function to check and rotate log files
+rotate_log_file() {
+    if [[ $(wc -l < "$CURRENT_FILE") -ge $LINE_LIMIT ]]; then
+        ((FILE_COUNT++))
+        CURRENT_FILE="$LOG_DIR/$(date +"%Y-%m-%d_%H-%M")_log_$FILE_COUNT.txt"
+        echo "Switched to new log file: $CURRENT_FILE"
+    fi
+}
+
+# Start the script command with logging to the initial file
+echo "Starting logging to $CURRENT_FILE"
+script -q -c "/bin/zsh" "$CURRENT_FILE" | while read -r line; do
+    rotate_log_file
+done
