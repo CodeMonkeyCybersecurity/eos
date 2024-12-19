7ba772781db27d326c6d954bc333326403f493fd
Code Monkey Cybersecurity <git@cybermonkey.net.au>
2024-12-19 17:31:42 +0800

    Update createTailscaleHostsYaml.sh
    Signed-off-by: Code Monkey Cybersecurity <git@cybermonkey.net.au>

--- scripts/createTailscaleHostsYaml.sh
+++ scripts/createTailscaleHostsYaml.sh
@@ -4,11 +4,17 @@
 set -xe
 
 ../utils/checkSudo.sh
-../utils/cyberMonkeyDir.sh
+CYBERMONKEY_DIR=$("../utils/cyberMonkeyDir.sh") || { echo "cyberMonkeyDir.sh failed"; exit 1; }
 
+# Define output file
 TAILSCALE_HOSTS_YAML="$CYBERMONKEY_DIR/tailscaleHosts.yaml"
 
+# Generate Tailscale hosts YAML
+echo "Generating Tailscale hosts YAML file..."
 tailscale status --json | jq -r '.Peer[] | "- hostname: \(.HostName)\n  ip: \(.TailAddr)"' | grep -v "$(hostname)" > "$TAILSCALE_HOSTS_YAML"
+
+# Completion message
+echo "Tailscale hosts YAML file created successfully at $TAILSCALE_HOSTS_YAML"
 
 set +x
 
