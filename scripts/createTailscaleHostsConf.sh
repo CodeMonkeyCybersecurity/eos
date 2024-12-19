5cccd1cb936d8e611d392ee667e97e06adb6513e
Code Monkey Cybersecurity <git@cybermonkey.net.au>
2024-12-19 19:18:26 +0800

    Update and rename createTailscaleHostsYaml.sh to createTailscaleHostsConf.sh
    Signed-off-by: Code Monkey Cybersecurity <git@cybermonkey.net.au>

--- scripts/createTailscaleHostsConf.sh
+++ scripts/createTailscaleHostsConf.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-# /scripts/createTailscaleHostsYaml.sh
+# /scripts/createTailscaleHostsConf.sh
 set -xe
 ../utils/checkSudo.sh
 ../utils/cyberMonkeyDir.sh
+source ../variables.conf
 # Define output file
-TAILSCALE_HOSTS_YAML="/opt/cyberMonkey/tailscaleHosts.yaml"
 # Generate Tailscale hosts YAML
 echo "Generating Tailscale hosts YAML file..."
 tailscale status --json | jq -r '.Peer[] | "- hostname: \(.HostName)\n  ip: \(.TailAddr)"' | grep -v "$(hostname)" > "$TAILSCALE_HOSTS_YAML"
-ls -lah "$TAILSCALE_HOSTS_YAML"
-cat "$TAILSCALE_HOSTS_YAML"
+ls -lah "$TAILSCALE_HOSTS_CONF"
+cat "$TAILSCALE_HOSTS_CONF"
 set +x
 echo "finis"
