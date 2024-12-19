7be83ef4183dc4928efa07be483e1e1a21eba485
Code Monkey Cybersecurity <git@cybermonkey.net.au>
2024-12-19 18:57:34 +0800

    Update createHostsYaml.sh
    Signed-off-by: Code Monkey Cybersecurity <git@cybermonkey.net.au>

--- scripts/createHostsYaml.sh
+++ scripts/createHostsYaml.sh
@@ -1,33 +1,25 @@
 #!/bin/bash
-# createHostsYaml.sh
-
+# /scripts/createHostsYaml.sh
 set -xe
-
 ../utils/checkSudo.sh
-
+../utils/cyberMonkeyDir.sh
+../variables.conf
 # Prompt for user input
 read -p "Enter the endpoints you want to manage (comma-separated, e.g., host1,host2,host3): " ENDPOINTS
 read -p "Enter the username you want to manage these with: " USER
-
-# Define YAML directory and file
-YAML_DIR="/opt/cyberMonkey"
-YAML_FILE="$YAML_DIR/hosts.yaml"
-
-# Create the directory and file if they don't exist
-mkdir -p "$YAML_DIR"
-
-# Write to YAML file
+# Write to .conf file
 {
-    echo "user: $USER"
-    echo "hosts:"
+    echo "user=$USER"
+    echo -n "hosts=\""
     IFS=',' read -ra HOST_ARRAY <<< "$ENDPOINTS"
     for HOST in "${HOST_ARRAY[@]}"; do
-        echo "  - $HOST"
+        echo -n "$HOST "
     done
-} > "$YAML_FILE"
+    echo "\""
+} > "$CONF_FILE"
 
-echo "YAML file created at $YAML_FILE with the following content:"
-cat "$YAML_FILE"
+echo ".conf file created at $CONF_FILE with the following content:"
+cat "$CONF_FILE"
 
 set +x
 
