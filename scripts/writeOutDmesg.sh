74aabbf808ea885fb78bfa985db3c110379d91b4
Code Monkey Cybersecurity <git@cybermonkey.net.au>
2024-12-19 18:07:45 +0800

    Update writeOutDmesg.sh
    Signed-off-by: Code Monkey Cybersecurity <git@cybermonkey.net.au>

--- scripts/writeOutDmesg.sh
+++ scripts/writeOutDmesg.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
-
 set -xe
-
 ../utils/checkSudo.sh
-
-DMESGS_DIR="/opt/cyberMonkey/dmesgs"
-
+source ../variables.conf
 mkdir -p "$DMESGS_DIR"
-
 dmesg > "$DMESGS_DIR/$(date +"%Y-%m-%d_%H-%M")_$(hostname)_dmesg.txt"
-
 ls -lah "$DMESGS_DIR"
-
 set +x
-
 echo "finis"
