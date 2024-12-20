a83147de11bc019621e71c7469c8082a427c25fa
henry <henry@cybermonkey.net.au>
2024-12-20 21:19:17 +0800

    kfvdkljndfagkl

--- softwareAndPackages/PostgreSQL/installPostgreSQL.sh
+++ softwareAndPackages/PostgreSQL/installPostgreSQL.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # /softwareAndPackages/PostgreSQL/installPostgreSQL.sh
-SCRIPT_DIR=$(dirname "$(realpath "$0")")
-source "$SCRIPT_DIR/../../utilities/start.sh" || { echo "Failed to source start.sh"; exit 1; }
+PROJECT_ROOT=$(realpath "$(dirname "$0")/../..")
+source "$UTILITIES_DIR/start.sh" || { echo "Failed to source start.sh"; exit 1; }
 apt install -y postgresql || { echo "Failed to install PostgreSQL"; exit 1; }
-source "$SCRIPT_DIR/../../utilities/stop.sh" || { echo "Failed to source stop.sh"; exit 1; }
+source "$UTILITIES_DIR/start.sh" || { echo "Failed to source stop.sh"; exit 1; }
--- utilities/start.sh
+++ utilities/start.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-source "$SCRIPT_DIR/checkSudo.sh"
+# /utilities/start.sh
+# Resolve the base directory of the current script
+PROJECT_ROOT=$(realpath "$(dirname "${BASH_SOURCE[0]}")/../")
+echo "PROJECT_ROOT: $PROJECT_ROOT"
+echo ""
+UTILITIES_DIR="$PROJECT_ROOT/utilities"
+echo "UTILITIES_DIR: $UTILITIES_DIR"
+echo ""
+source "$UTILITIES_DIR/checkSudo.sh" # Source checkSudo.sh
 set -xe
-# Resolve the directory of the current script
-VARIABLES_CONF="$(cd "$SCRIPT_DIR/.." && pwd)/variables.conf"
+echo ""
+VARIABLES_CONF="$PROJECT_ROOT/variables.conf"
+echo ""
 source "$VARIABLES_CONF"
-# Source log.sh using an absolute path
-source "$SCRIPT_DIR/timestamp.sh"
-source "$SCRIPT_DIR/userHostnameStamp.sh"
-source "$SCRIPT_DIR/stamp.sh"
-source "$SCRIPT_DIR/log.sh"
+echo ""
+source "$UTILITIES_DIR/timestamp.sh"
+echo ""
+source "$UTILITIES_DIR/userHostnameStamp.sh"
+echo ""
+source "$UTILITIES_DIR/stamp.sh"
+echo ""
+source "$UTILITIES_DIR/log.sh"
