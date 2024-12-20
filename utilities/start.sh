505c57b5081157fa3cbde1a137072653ff1a9c90
henry <henry@cybermonkey.net.au>
2024-12-20 17:54:37 +0800

    dsvafzdfdfs

--- utilities/start.sh
+++ utilities/start.sh
@@ -2,7 +2,6 @@
 set -xe
 # Resolve the directory of the current script
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-source "$SCRIPT_DIR/checkSudo.sh"
 VARIABLES_CONF="$(cd "$SCRIPT_DIR/.." && pwd)/variables.conf"
 source "$VARIABLES_CONF"
 # Source log.sh using an absolute path
