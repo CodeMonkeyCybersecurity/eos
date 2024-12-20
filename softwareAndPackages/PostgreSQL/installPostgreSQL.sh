056763e14b791d4cc1fa2f9971e648750f9d42b1
henry <henry@cybermonkey.net.au>
2024-12-20 21:21:08 +0800

    dkjdsal

--- softwareAndPackages/PostgreSQL/installPostgreSQL.sh
+++ softwareAndPackages/PostgreSQL/installPostgreSQL.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # /softwareAndPackages/PostgreSQL/installPostgreSQL.sh
-PROJECT_ROOT=$(realpath "$(dirname "$0")/../..")
+PROJECT_ROOT=$(realpath "$(dirname "$0")/../..") || { echo "Failed to source PROJECT_ROOT"; exit 1; }
 source "$UTILITIES_DIR/start.sh" || { echo "Failed to source start.sh"; exit 1; }
 apt install -y postgresql || { echo "Failed to install PostgreSQL"; exit 1; }
 source "$UTILITIES_DIR/start.sh" || { echo "Failed to source stop.sh"; exit 1; }
