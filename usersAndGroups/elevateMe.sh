b0aca34a4695a44a3e637e7bdfd512f155db80ec
henry <henry@cybermonkey.net.au>
2024-12-20 17:58:44 +0800

    gaafgadfgadfggf

--- usersAndGroups/elevateMe.sh
+++ usersAndGroups/elevateMe.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
-
 # elevates you to root, then redirects you to your current directory
-
 # Variables
+source ../utilities/checkSudo.sh
+source ../utilities/start.sh
 PWD=$(pwd)
 echo "Current directory: $PWD"
-
 sudo bash -c "cd '$PWD'; exec bash"  # Use sudo bash -c to run commands as root in the same shell 
-
 echo "you are: $(root)"
 echo "Your new current directory is: $PWD"
-
-echo "done"
-
-
-
+source ../utilities/stop.sh
