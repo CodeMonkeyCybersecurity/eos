9aada402857215384ae680b90bf12cf6991a3547
henry <henry@cybermonkey.net.au>
2024-12-20 21:50:07 +0800

    adfgsfgsfd

--- softwareAndPackages/PostgreSQL/installPostgreSQL.sh
+++ softwareAndPackages/PostgreSQL/installPostgreSQL.sh
@@ -6,6 +6,7 @@ echo ""
 UTILITIES_DIR="$PROJECT_ROOT/utilities" || { echo "Failed to source UTILITIES_DIR"; exit 1; }
 echo "UTILITIES_DIR: $UTILITIES_DIR"
 echo ""
+source "$UTILITIES_DIR/apt.sh"
 source "$UTILITIES_DIR/start.sh" || { echo "Failed to source start.sh"; exit 1; }
 apt install -y postgresql || { echo "Failed to install PostgreSQL"; exit 1; }
 source "$UTILITIES_DIR/stop.sh" || { echo "Failed to source stop.sh"; exit 1; }
