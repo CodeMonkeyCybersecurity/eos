0280a2abc0454d9ffef1582a90513f9b3eb7afea
henry <henry@cybermonkey.net.au>
2024-12-20 20:06:39 +0800

    fdsgsfdgsdg

--- softwareAndPackages/PostgreSQL/installPostgreSQL.sh
+++ softwareAndPackages/PostgreSQL/installPostgreSQL.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # /softwareAndPackages/PostgreSQL/installPostgreSQL.sh
-source ../../utilities/start.sh
-apt install postgresql
-source ../../utilities/stop.sh
+source ../../utilities/start.sh || { echo "Failed to source start.sh"; exit 1; }
+apt install -y postgresql || { echo "Failed to install PostgreSQL"; exit 1; }
+source ../../utilities/stop.sh || { echo "Failed to source stop.sh"; exit 1; }
