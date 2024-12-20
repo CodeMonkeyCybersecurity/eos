f177f06d6554aa7b0c6d27133c3d9ea761ea7694
codeMonkeyCybersecurity <git@cybermonkey.net.au>
2024-12-20 20:14:13 +0800

    Update installPostgreSQL.sh
    Signed-off-by: codeMonkeyCybersecurity <git@cybermonkey.net.au>

--- softwareAndPackages/PostgreSQL/installPostgreSQL.sh
+++ softwareAndPackages/PostgreSQL/installPostgreSQL.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # /softwareAndPackages/PostgreSQL/installPostgreSQL.sh
-source ../../utilities/start.sh || { echo "Failed to source start.sh"; exit 1; }
+SCRIPT_DIR=$(dirname "$(realpath "$0")")
+source "$SCRIPT_DIR/../../utilities/start.sh" || { echo "Failed to source start.sh"; exit 1; }
 apt install -y postgresql || { echo "Failed to install PostgreSQL"; exit 1; }
-source ../../utilities/stop.sh || { echo "Failed to source stop.sh"; exit 1; }
+source "$SCRIPT_DIR/../../utilities/stop.sh" || { echo "Failed to source stop.sh"; exit 1; }
