15a5963c1b61c606ce4b5508daaa3917e2be488d
henry <henry@cybermonkey.net.au>
2024-12-21 19:04:49 +0800

    jvydfjv,

--- cmd/eos/get.go
+++ cmd/eos/get.go
@@ -1,7 +1,4 @@
-/*
-Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
-*/
+// get.go
 package cmd
 
 import (
@@ -10,31 +7,12 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// getCmd represents the get command
+// getCmd represents the "get" subcommand
 var getCmd = &cobra.Command{
 	Use:   "get",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Retrieve information about resources (processes, users, backups, etc.)",
+	Long:  `Use eos get to retrieve detailed information about system resources, such as processes, users, backups, etc.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("get called")
+		fmt.Println("You called `eos get` without specifying a resource.")
 	},
-}
-
-func init() {
-	rootCmd.AddCommand(getCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// getCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// getCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
--- cmd/eos/root.go
+++ cmd/eos/root.go
@@ -1,30 +1,25 @@
 /*
 Copyright © 2024 Henry Oliver henry@cybermonkey.net.au
-
 */
 package cmd
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
 )
 
-
-
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "eos",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Short: "Eos CLI for managing local and remote environments",
+	Long: `Eos is a command-line application for managing processes, users,
+hardware, backups, and more.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		// Default action when no subcommand is provided
+		fmt.Println("Eos CLI: Use 'eos --help' to see available commands.")
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -36,16 +31,20 @@ func Execute() {
 	}
 }
 
+var cfgFile string
+
 func init() {
 	// Here you will define your flags and configuration settings.
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
 
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.eos.yaml)")
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.eos.yaml)")
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.AddCommand(getCmd)
+	rootCmd.AddCommand(setCmd)
+	rootCmd.AddCommand(runCmd)
+	rootCmd.AddCommand(deleteCmd)
 }
-
-
