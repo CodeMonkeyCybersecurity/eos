46d38f65bf3f44ea5a7a67750c9ed1ddc3e093c8
henry <henry@cybermonkey.net.au>
2024-12-21 22:55:50 +0800

    modified:   cmd/root.go

--- cmd/root.go
+++ cmd/root.go
@@ -4,9 +4,13 @@ Copyright © 2024 Henry Oliver henry@cybermonkey.net.au
 package cmd
 
 import (
+	"database/sql"
+	"eos/pkg/utils"
 	"fmt"
+	"log"
 	"os"
 
+	_ "github.com/lib/pq" // PostgreSQL driver
 	"github.com/spf13/cobra"
 )
 
@@ -25,10 +29,10 @@ hardware, backups, and more.`,
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
-	err := rootCmd.Execute()
-	if err != nil {
-		os.Exit(1)
-	}
+    if err := rootCmd.Execute(); err != nil {
+        utils.GetLogger().Error(fmt.Sprintf("Command execution failed: %v", err))
+        os.Exit(1)
+    }
 }
 
 var cfgFile string
@@ -37,12 +41,34 @@ func init() {
 	// Here you will define your flags and configuration settings.
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
-
 	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.eos.yaml)")
+	// Database connection details
+	dbHost := "localhost"
+	dbPort := "5432"
+	dbUser := "postgres"
+	dbPassword := os.Getenv("DB_PASSWORD")
+	if dbPassword == "" {
+		log.Fatal("Database password not set. Please set the DB_PASSWORD environment variable.")
+	}	dbName := "eos_db"
+	// Connection string
+	connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		dbHost, dbPort, dbUser, dbPassword, dbName)
 
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// Connect to PostgreSQL
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatalf("Failed to connect to PostgreSQL: %v", err)
+	}
+	// Check database health
+	if err := db.Ping(); err != nil {
+		log.Fatalf("Failed to connect to the database: %v", err)
+	}
+	// Initialize the global logger
+	err = utils.InitializeLogger(db, "/var/log/eos.log", utils.InfoLevel, true)
+	if err != nil {
+		log.Fatalf("Failed to initialize logger: %v", err)
+	}
+	// Add subcommands
 	rootCmd.AddCommand(getCmd)
 	rootCmd.AddCommand(setCmd)
 	rootCmd.AddCommand(goCmd)
