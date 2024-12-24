f4fa55556922b0e97fa530a61abe5b25642eaf2b
henry <henry@cybermonkey.net.au>
2024-12-24 18:49:17 +0800

    new file:   retrieveYaml.go

---
+++ retrieveYaml.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"gopkg.in/yaml.v3"
+)
+
+// Config represents the structure of the YAML configuration file
+type Config struct {
+	Database struct {
+		User     string `yaml:"user"`
+		Password string `yaml:"password"`
+		Host     string `yaml:"host"`
+		Port     int    `yaml:"port"`
+	} `yaml:"database"`
+	Logging struct {
+		Level string `yaml:"level"`
+		File  string `yaml:"file"`
+	} `yaml:"logging"`
+}
+
+func main() {
+	// Specify the path to your YAML file
+	yamlFilePath := "config.yaml"
+
+	// Read the YAML file
+	file, err := os.ReadFile(yamlFilePath)
+	if err != nil {
+		log.Fatalf("Error reading YAML file: %v\n", err)
+	}
+
+	// Parse the YAML content into the Config struct
+	var config Config
+	err = yaml.Unmarshal(file, &config)
+	if err != nil {
+		log.Fatalf("Error parsing YAML file: %v\n", err)
+	}
+
+	// Access and print values from the parsed config
+	fmt.Printf("Database User: %s\n", config.Database.User)
+	fmt.Printf("Database Host: %s\n", config.Database.Host)
+	fmt.Printf("Log Level: %s\n", config.Logging.Level)
+	fmt.Printf("Log File: %s\n", config.Logging.File)
+}
