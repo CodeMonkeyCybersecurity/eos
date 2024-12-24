862bd942560d028078c433208ad1f1f28ac5c823
henry <henry@cybermonkey.net.au>
2024-12-24 21:20:49 +0800

    modified:   retrieveYaml.go

--- retrieveYaml.go
+++ retrieveYaml.go
@@ -24,7 +24,7 @@ type Config struct {
 
 func main() {
 	// Specify the path to your YAML file
-	yamlFilePath := "eos/config/default.yaml"
+	yamlFilePath := "config/default.yaml"
 
 	// Read the YAML file
 	file, err := os.ReadFile(yamlFilePath)
