de095e3742a51a4b013ffdbe29b50f937cfaa489
henry <henry@cybermonkey.net.au>
2024-12-24 21:33:46 +0800

    modified:   retrieveYaml.go

--- retrieveYaml.go
+++ retrieveYaml.go
@@ -11,10 +11,11 @@ import (
 // Config represents the structure of the YAML configuration file
 type Config struct {
 	Database struct {
-		Name string `yaml:"name"`
-		User string `yaml:"user"`
-		Host string `yaml:"host"`
-		Port string `yaml:"port"`
+		Name    string `yaml:"name"`
+		User    string `yaml:"user"`
+		Host    string `yaml:"host"`
+		Port    string `yaml:"port"`
+		Version string `yaml:"version"`
 	} `yaml:"database"`
 	Logging struct {
 		Level string `yaml:"level"`
@@ -44,6 +45,7 @@ func main() {
 	fmt.Printf("Database User: %s\n", config.Database.User)
 	fmt.Printf("Database Host: %s\n", config.Database.Host)
 	fmt.Printf("Database Port: %s\n", config.Database.Port)
+	fmt.Printf("Database Version: %s\n", config.Database.Version)
 	fmt.Printf("Log Level: %s\n", config.Logging.Level)
 	fmt.Printf("Log File: %s\n", config.Logging.File)
 }
