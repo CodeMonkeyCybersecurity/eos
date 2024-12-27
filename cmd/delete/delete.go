7d4da77833474bb01960f14ee75b23adaea19809
codeMonkeyCybersecurity <git@cybermonkey.net.au>
2024-12-27 11:12:04 +0800

    Update delete.go
    Signed-off-by: codeMonkeyCybersecurity <git@cybermonkey.net.au>

--- cmd/delete/delete.go
+++ cmd/delete/delete.go
@@ -23,27 +23,3 @@ func init() {
 	DeleteCmd.AddCommand(deleteUsersCmd)
 	DeleteCmd.AddCommand(deleteStorageCmd)
 }
-
-var deleteProcessesCmd = &cobra.Command{
-	Use:   "processes",
-	Short: "Delete a process",
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Deleting a process...")
-	},
-}
-
-var deleteUsersCmd = &cobra.Command{
-	Use:   "users",
-	Short: "Delete a user",
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Deleting a user...")
-	},
-}
-
-var deleteStorageCmd = &cobra.Command{
-	Use:   "storage",
-	Short: "Delete storage",
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Deleting storage...")
-	},
-}
