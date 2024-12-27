6c7d16fb6954456c89ed741f46171f0fb1a7e560
henry <henry@cybermonkey.net.au>
2024-12-27 16:28:45 +0800

    modified:   main.go

--- main.go
+++ main.go
@@ -3,8 +3,15 @@ Copyright © 2024 Code Monkey Cybersecurity mailto:git@cybermonkey.net.au
 */
 package main
 
-import "eos/cmd"
+import (
+	"eos/cmd"
+	"eos/pkg/logger"
+)
 
 func main() {
+	logger.Initialize()
+	defer logger.Sync()
+	logger.GetLogger().Info("Eos CLI initialized and ready.")
+
 	cmd.Execute()
 }
