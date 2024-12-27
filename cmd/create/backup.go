52d7dc61f0bc8a3bc4924a9ab7d2e0cb2f697d59
henry <henry@cybermonkey.net.au>
2024-12-27 11:50:33 +0800

    modified:   cmd/create/backup.go

--- cmd/create/backup.go
+++ cmd/create/backup.go
@@ -21,7 +21,7 @@ var createBackupCmd = &cobra.Command{
 }
 
 func init() {
-	create.AddCommand(createBackupCmd)
+	createCmd.AddCommand(createBackupCmd)
 
 	// Here you will define your flags and configuration settings.
 
