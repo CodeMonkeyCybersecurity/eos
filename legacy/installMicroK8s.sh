e883ac35db88854d3a401fd11b0f7f6e6d71c58c
henry <henry@cybermonkey.net.au>
2024-12-29 16:06:26 +0800

    modified:   legacy/installMicroK8s.sh 	renamed:    installQemuGuestAgent.sh -> legacy/installQemuGuestAgent.sh

--- legacy/installMicroK8s.sh
+++ legacy/installMicroK8s.sh
@@ -5,9 +5,10 @@ sudo ufw allow 25000:25010/tcp && sudo ufw reload
 # https://microk8s.io/docs/getting-started
 sudo snap install microk8s --classic
 sudo usermod -a -G microk8s $USER
+sudo chown -R henry ~/.kube
 mkdir -p ~/.kube
 chmod 0700 ~/.kube
-su - $USER
+newgrp microk8s
 
 sudo microk8s status --wait-ready
 
@@ -21,9 +22,11 @@ sudo microk8s kubectl get all --all-namespaces
 
 sudo microk8s dashboard-proxy
 
+exit # to allow for permissions chagens above to take effect
+
 
 ----------
-https://snapcraft.io/install/microceph/ubuntu
+# https://snapcraft.io/install/microceph/ubuntu
 sudo snap install microceph
  sudo snap refresh --hold microceph
  sudo microceph cluster bootstrap
@@ -32,45 +35,12 @@ sudo snap install microceph
  kubectl apply -f https://raw.githubusercontent.com/ceph/ceph-csi/devel/examples/kubernetes/cephfs/driver.yaml
 
 
+-------------
+# https://microk8s.io/docs/how-to-ceph
+sudo snap install microceph --channel=latest/edge
+sudo microceph cluster bootstrap
+sudo microceph.ceph status                                                                                                                                                                                        
+sudo microceph disk list                                                                    
+sudo microk8s enable rook-ceph
+sudo microk8s connect-external-ceph
 
-
-
-
-
-
-
-
-
- --------------
- henry@vhost1:~$ sudo microk8s status
-microk8s is running
-high-availability: no
-  datastore master nodes: 100.122.41.58:19001
-  datastore standby nodes: none
-addons:
-  enabled:
-    dashboard            # (core) The Kubernetes dashboard
-    dns                  # (core) CoreDNS
-    ha-cluster           # (core) Configure high availability on the current node
-    helm                 # (core) Helm - the package manager for Kubernetes
-    helm3                # (core) Helm 3 - the package manager for Kubernetes
-    hostpath-storage     # (core) Storage class; allocates storage from host directory
-    ingress              # (core) Ingress controller for external access
-    metrics-server       # (core) K8s Metrics Server for API access to service metrics
-    registry             # (core) Private image registry exposed on localhost:32000
-    storage              # (core) Alias to hostpath-storage add-on, deprecated
-  disabled:
-    cert-manager         # (core) Cloud native certificate management
-    cis-hardening        # (core) Apply CIS K8s hardening
-    community            # (core) The community addons repository
-    gpu                  # (core) Alias to nvidia add-on
-    host-access          # (core) Allow Pods connecting to Host services smoothly
-    kube-ovn             # (core) An advanced network fabric for Kubernetes
-    mayastor             # (core) OpenEBS MayaStor
-    metallb              # (core) Loadbalancer for your Kubernetes cluster
-    minio                # (core) MinIO object storage
-    nvidia               # (core) NVIDIA hardware (GPU and network) support
-    observability        # (core) A lightweight observability stack for logs, traces and metrics
-    prometheus           # (core) Prometheus operator for monitoring and logging
-    rbac                 # (core) Role-Based Access Control for authorisation
-    rook-ceph            # (core) Distributed Ceph storage using Rook
