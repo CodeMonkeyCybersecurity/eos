859b1a788acca0d2d739cfe7a16fbc3825e9400c
henry <henry@cybermonkey.net.au>
2024-12-30 22:24:39 +0800

    modified:   legacy/installMicroK8s.sh

--- legacy/installMicroK8s.sh
+++ legacy/installMicroK8s.sh
@@ -4,17 +4,15 @@ sudo ufw allow 25000:25010/tcp && sudo ufw reload
 
 # https://microk8s.io/docs/getting-started
 sudo snap install microk8s --classic
-sudo usermod -a -G microk8s $USER
+sudo usermod -a -G microk8s henry
 sudo chown -R henry ~/.kube
 newgrp microk8s
 
 sudo microk8s status --wait-ready
 
 
-sudo microk8s enable dashboard dns  registry  istio rook-ceph
-
+sudo microk8s enable dashboard dns rbac registry  istio rook-ceph
 sudo microk8s enable ingress
-
 
 sudo microk8s kubectl get all --all-namespaces
 
