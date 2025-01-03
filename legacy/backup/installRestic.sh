67c779e4bfe5fc9e468546c32d0c921c4ed4a0ca
henry <henry@cybermonkey.net.au>
2025-01-04 02:10:30 +0800

    modified:   legacy/backup/installRestic.sh

--- legacy/backup/installRestic.sh
+++ legacy/backup/installRestic.sh
@@ -11,8 +11,6 @@ sudo restic -r sftp:henry@backup:/srv/restic-repos/$(hostname) init
 # backup 
 sudo restic -r sftp:henry@backup:/srv/restic-repos/$(hostname) --verbose backup /root /home /var /etc /srv /usr /opt
 
-read -p "What is your restic repo password?: " RESTIC_PASS
-
 echo "$RESTIC_PASS" | sudo tee /root/.restic-password > /dev/null
 sudo chmod 600 /root/.restic-password
 RESTIC_REPO="sftp:henry@backup:/srv/restic-repos/$(hostname)"
