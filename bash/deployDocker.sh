56b446f9a64d7f1016b053e233a15a51a12e35a0
codeMonkeyCybersecurity <git@cybermonkey.net.au>
2025-02-03 16:05:37 +0800

    Update deployDocker.sh
    Signed-off-by: codeMonkeyCybersecurity <git@cybermonkey.net.au>

--- bash/deployDocker.sh
+++ bash/deployDocker.sh
@@ -4,6 +4,11 @@
 echo "uninstalling conflicting packages"
 for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
 
+echo "updating apt repos"
+sudo apt update 
+sudo apt autoremove --purge
+sudo apt autoclean
+
 echo "Add Docker's official GPG key"
 sudo apt-get update
 sudo apt-get install ca-certificates curl
