19371b67449112605705c357883685d7a2cc6cee
codeMonkeyCybersecurity <git@cybermonkey.net.au>
2025-02-03 16:02:01 +0800

    Create deployDocker.sh
    Signed-off-by: codeMonkeyCybersecurity <git@cybermonkey.net.au>

---
+++ bash/deployDocker.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# deployDocker.sh
+
+echo "uninstalling conflicting packages"
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
+
+echo "Add Docker's official GPG key"
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+echo "Add the repository to Apt sources"
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+echo "Install the latest version"
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+echo "Verify that the installation is successful by running the hello-world image"
+sudo docker run hello-world
+
+echo "running Linux post-installation steps for Docker Engine to manage Docker as a non-root user"
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker
+
+echo "now verifying docker can run without sudo"
+docker run hello-world
+
+echo "finis"
+
