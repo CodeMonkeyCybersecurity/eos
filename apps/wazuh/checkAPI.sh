722f198bbc3a6c0646e3bc6b53aed537a15d5499
henry <git@cybermonkey.net.au>
2025-02-06 14:03:17 +0000

    modified:   checkAPI.sh

--- apps/wazuh/checkAPI.sh
+++ apps/wazuh/checkAPI.sh
@@ -12,7 +12,7 @@ echo "  CHECK API  "
 echo "============="
 
 # Where we store the last-used values
-LAST_VALUES_FILE=".wazuh.conf"
+LAST_VALUES_FILE=".wazuh.api.conf"
 
 # If we have a saved file from a previous run, load it.
 if [[ -f "$LAST_VALUES_FILE" ]]; then
@@ -24,7 +24,7 @@ fi
 prompt_input() {
     local var_name=$1
     local prompt_message=$2
-    local default_val=${!var_name}  
+    local default_val=${!var_name}
     local input
 
     while true; do
@@ -50,21 +50,22 @@ echo ""
 echo "=== NGINX Configuration Generator ==="
 
 # Prompt for values (will show defaults if any)
-WZ_FQDN=$(prompt_input "WZ_FQDN" "Enter the Wazuh domain (eg. wazuh.domain.com):")
+WZ_FQDN=$(prompt_input "WZ_FQDN" "Enter the Wazuh domain (eg. wazuh.domain.com): ")
 WZ_API_USR=$(prompt_input "WZ_API_USR" "Enter the API username (eg. wazuh-wui): ")
 WZ_API_PASSWD=$(prompt_input "WZ_API_PASSWD" "Enter the API passwd: ")
 
 # Save the values so future runs start with the same defaults
-cat <<EOF > "$LAST_VALUES_FILE"
-WZ_FQDN="$WZ_FQDN"
-WZ_API_USR="$WZ_API_USR"
-WZ_API_PASSWD="$WZ_API_PASSWD"
-EOF
-
 echo ""
 echo "Retrieving JWT token..."
 
 TOKEN=$(curl -u "${WZ_API_USR}:${WZ_API_PASSWD}" -k -X POST "https://${WZ_FQDN}:55000/security/user/authenticate?raw=true")
+
+cat <<EOF > "$LAST_VALUES_FILE"
+WZ_FQDN="$WZ_FQDN"
+WZ_API_USR="$WZ_API_USR"
+WZ_API_PASSWD="$WZ_API_PASSWD"
+TOKEN="$TOKEN"
+EOF
 
 echo ""
 echo "Your JWT auth token is:"
