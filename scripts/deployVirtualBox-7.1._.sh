7968d9b5ce5e863dc12f74f1cba7e8a6666f8f2a
codeMonkeyCybersecurity <git@cybermonkey.net.au>
2025-02-15 10:29:07 +0800

    Update deployVirtualBox-7.1._.sh
    Signed-off-by: codeMonkeyCybersecurity <git@cybermonkey.net.au>

--- scripts/deployVirtualBox-7.1._.sh
+++ scripts/deployVirtualBox-7.1._.sh
@@ -7,7 +7,7 @@ echo "add oracle GPG key"
 wget -qO- https://www.virtualbox.org/download/oracle_vbox_2016.asc | sudo tee /etc/apt/trusted.gpg.d/oracle-virtualbox.asc
 
 echo ""
-echo "adding VirtualBox repositor"
+echo "adding VirtualBox repository"
 echo "deb [arch=amd64] https://download.virtualbox.org/virtualbox/debian $(lsb_release -cs) contrib" | sudo tee /etc/apt/sources.list.d/virtualbox.list
 
 
