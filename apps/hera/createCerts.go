30ab928e7909b701c93586367f634abf810879a7
codeMonkeyCybersecurity <git@cybermonkey.net.au>
2025-03-01 22:12:13 +0800

    Update createCerts.go
    Signed-off-by: codeMonkeyCybersecurity <git@cybermonkey.net.au>

--- apps/hera/createCerts.go
+++ apps/hera/createCerts.go
@@ -9,10 +9,18 @@ import (
 	"fmt"
 	"math/big"
 	"os"
+	"path/filepath"
 	"time"
 )
 
 func main() {
+	// Create the "certs" directory if it doesn't exist.
+	certsDir := "certs"
+	if err := os.MkdirAll(certsDir, 0755); err != nil {
+		fmt.Printf("Failed to create directory %s: %v\n", certsDir, err)
+		os.Exit(1)
+	}
+
 	// Generate a private key.
 	priv, err := rsa.GenerateKey(rand.Reader, 2048)
 	if err != nil {
@@ -48,30 +56,32 @@ func main() {
 		os.Exit(1)
 	}
 
-	// Write certificate to tls.crt.
-	certOut, err := os.Create("tls.crt")
+	// Write certificate to certs/tls.crt.
+	certPath := filepath.Join(certsDir, "tls.crt")
+	certOut, err := os.Create(certPath)
 	if err != nil {
-		fmt.Printf("Failed to open tls.crt for writing: %v\n", err)
+		fmt.Printf("Failed to open %s for writing: %v\n", certPath, err)
 		os.Exit(1)
 	}
 	defer certOut.Close()
 	if err := pem.Encode(certOut, &pem.Block{Type: "CERTIFICATE", Bytes: derBytes}); err != nil {
-		fmt.Printf("Failed to write data to tls.crt: %v\n", err)
+		fmt.Printf("Failed to write data to %s: %v\n", certPath, err)
 		os.Exit(1)
 	}
-	fmt.Println("Written tls.crt")
+	fmt.Printf("Written %s\n", certPath)
 
-	// Write private key to tls.key.
-	keyOut, err := os.OpenFile("tls.key", os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0600)
+	// Write private key to certs/tls.key.
+	keyPath := filepath.Join(certsDir, "tls.key")
+	keyOut, err := os.OpenFile(keyPath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0600)
 	if err != nil {
-		fmt.Printf("Failed to open tls.key for writing: %v\n", err)
+		fmt.Printf("Failed to open %s for writing: %v\n", keyPath, err)
 		os.Exit(1)
 	}
 	defer keyOut.Close()
 	privBytes := x509.MarshalPKCS1PrivateKey(priv)
 	if err := pem.Encode(keyOut, &pem.Block{Type: "RSA PRIVATE KEY", Bytes: privBytes}); err != nil {
-		fmt.Printf("Failed to write data to tls.key: %v\n", err)
+		fmt.Printf("Failed to write data to %s: %v\n", keyPath, err)
 		os.Exit(1)
 	}
-	fmt.Println("Written tls.key")
+	fmt.Printf("Written %s\n", keyPath)
 }
