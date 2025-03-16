2631c492d94462645a028f8dfa6bf7cfa81b28e4
codeMonkeyCybersecurity <git@cybermonkey.net.au>
2025-03-16 14:27:36 +0800

    Update secure.go
    Signed-off-by: codeMonkeyCybersecurity <git@cybermonkey.net.au>

--- cmd/secure/secure.go
+++ cmd/secure/secure.go
@@ -13,8 +13,3 @@ var SecureCmd = &cobra.Command{
 For example:
   eos secure Trivy  - Secures the Trivy vulnerability scanner.`,
 }
-
-// In the init function, attach subcommands (for example, the Vault Secure-er).
-func init() {
-	SecureCmd.AddCommand(trivyCmd) // trivyCmd is defined in the same package (in trivy.go)
-}
