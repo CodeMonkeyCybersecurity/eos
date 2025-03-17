04bd5bd7b04f6c00a28d1bda3899da762190472d
codeMonkeyCybersecurity <git@cybermonkey.net.au>
2025-03-17 13:27:13 +0800

    Create install.go
    Signed-off-by: codeMonkeyCybersecurity <git@cybermonkey.net.au>

---
+++ cmd/delphi/install/install.go
@@ -0,0 +1,21 @@
+// cmd/delphi/install/install.go
+package install
+
+import (
+	"github.com/spf13/cobra"
+  "eos/cmd/delphi/install/dockerlistener"
+)
+
+// InstallCmd is the root command for Delphi installation actions
+var InstallCmd = &cobra.Command{
+	Use:   "install",
+	Short: "Install Delphi components",
+	Long:  `Commands to install Wazuh/Delphi components like docker-listener.
+For example:
+  eos install delphi docker-listener`,
+}
+
+// In the init function, attach subcommands (for example, the Trivy installer).
+func init() {
+	InstallCmd.AddCommand(dockerListener.DockerListenerCmd)
+}
