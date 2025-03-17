4085eeb07f543a25fcbfe536d014a988e7ec1cae
codeMonkeyCybersecurity <git@cybermonkey.net.au>
2025-03-17 12:59:06 +0800

    Create config.go
    Signed-off-by: codeMonkeyCybersecurity <git@cybermonkey.net.au>

---
+++ pkg/config/config.go
@@ -0,0 +1,7 @@
+package config
+
+// DelphiListener virtual environment path
+const VenvPath = "/opt/delphi_venv"
+
+// Wazuh DockerListener script path
+const DockerListener = "/var/ossec/wodles/docker/DockerListener"
