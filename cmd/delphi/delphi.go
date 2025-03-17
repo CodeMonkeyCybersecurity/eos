cc94524cda1562d47755de8581c2d5ef61a1563f
codeMonkeyCybersecurity <git@cybermonkey.net.au>
2025-03-17 13:27:46 +0800

    Update delphi.go
    Signed-off-by: codeMonkeyCybersecurity <git@cybermonkey.net.au>

--- cmd/delphi/delphi.go
+++ cmd/delphi/delphi.go
@@ -1,1 +1,18 @@
+package delphi
 
+import (
+	"github.com/spf13/cobra"
+	"eos/cmd/delphi/install"
+)
+
+// DelphiCmd is the root command for Delphi-related actions
+var DelphiCmd = &cobra.Command{
+	Use:   "delphi",
+	Short: "Manage Delphi (Wazuh) components",
+	Long:  "Commands related to Wazuh and Delphi integrations such as install, remove, inspect.",
+}
+
+func init() {
+	// Register subcommands
+	DelphiCmd.AddCommand(install.InstallCmd)
+}
