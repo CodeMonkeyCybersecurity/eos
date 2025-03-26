bd5037900dde3adaee8113adeb31f7d6e7ad7931
codeMonkeyCybersecurity <git@cybermonkey.net.au>
2025-03-26 15:34:36 +0800

    Update config.go
    Signed-off-by: codeMonkeyCybersecurity <git@cybermonkey.net.au>

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -11,16 +11,16 @@ var Log *zap.Logger // Global logger
 
 // DelphiListener virtual environment path
 const (
-    VenvPath             = "/opt/delphi_venv"
-    DockerListener       = "/var/ossec/wodles/docker/DockerListener" // Wazuh DockerListener script path
-    UmamiDir             = "/opt/umami" // Umami install dir
-    JenkinsDir           = "/opt/jenkins" // Jenkins install dir
-    ZabbixDir            = "/opt/zabbix"
+    	VenvPath             = "/opt/delphi_venv"
+    	DockerListener       = "/var/ossec/wodles/docker/DockerListener" // Wazuh DockerListener script path
+    	UmamiDir             = "/opt/umami" // Umami install dir
+    	JenkinsDir           = "/opt/jenkins" // Jenkins install dir
+    	ZabbixDir            = "/opt/zabbix"
 	ZabbixComposeYML     = "/opt/zabbix/zabbix-docker-compose.yml"
-    MaxPreviewSize = 5 * 1024       // Treecat - 5KB max preview
-    MaxPreviewLines = 100           // Treecat - 100 lines max
+    	MaxPreviewSize = 5 * 1024       // Treecat - 5KB max preview
+    	MaxPreviewLines = 100           // Treecat - 100 lines max
 )
 
 func InitConfig() {
-    Log = logger.GetLogger() // Initialize logger once
+    	Log = logger.GetLogger() // Initialize logger once
 }
