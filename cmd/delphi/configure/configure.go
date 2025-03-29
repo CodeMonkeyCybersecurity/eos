1ed06984a0b4a2bd7c7f4c927b942ba900a081e1
chickenj0 <zyxwv@duck.com>
2025-03-29 16:28:10 +0800

    modified:   cmd/delphi/configure/configure.go

--- cmd/delphi/configure/configure.go
+++ cmd/delphi/configure/configure.go
@@ -6,6 +6,7 @@ import "github.com/spf13/cobra"
 var ConfigureCmd = &cobra.Command{
 	Use:   "configure",
 	Short: "Configure Delphi (Wazuh) related services",
+	Long:  "Run configuration commands such as setting up firewall rules, tuning agent settings, and more.",
 }
 
 func init() {
