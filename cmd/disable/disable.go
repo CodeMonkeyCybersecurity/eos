e35219e181bab748c842e5fb698d52fead8f5660
codeMonkeyCybersecurity <git@cybermonkey.net.au>
2025-04-06 15:31:46 +0800

    Update disable.go
    Signed-off-by: codeMonkeyCybersecurity <git@cybermonkey.net.au>

--- cmd/disable/disable.go
+++ cmd/disable/disable.go
@@ -3,12 +3,29 @@
 package disable
 
 import (
+	"fmt"
+
+	"github.com/CodeMonkeyCybersecurity/eos/pkg/flags"
+	"github.com/CodeMonkeyCybersecurity/eos/pkg/logger"
+
 	"github.com/spf13/cobra"
+	"go.uber.org/zap"
 )
 
 var DisableCmd = &cobra.Command{
 	Use:   "disable",
 	Short: "Disable system features (e.g., suspension, hibernation)",
+
+	RunE: func(cmd *cobra.Command, args []string) error {
+		log := logger.L()
+
+		if flags.IsDryRun() {
+			log.Info("Dry-run mode: no subcommand executed", zap.String("command", cmd.Use))
+			fmt.Printf("💡 [dry-run] No subcommand was executed for '%s'\n", cmd.Use)
+			return nil
+		}
+
+		log.Info("No subcommand provided for <command>.", zap.String("command", cmd.Use))
+		return cmd.Help()
+	},
 }
-
-
