9fc202d72378b093899a8932a5896f6f8d425307
codeMonkeyCybersecurity <git@cybermonkey.net.au>
2025-04-06 19:14:46 +0800

    Create hooks.go
    Signed-off-by: codeMonkeyCybersecurity <git@cybermonkey.net.au>

---
+++ pkg/undo/hooks.go
@@ -0,0 +1,18 @@
+// pkg/undo/hooks.go
+
+package undo
+
+import (
+	"github.com/CodeMonkeyCybersecurity/eos/pkg/flags"
+)
+
+// FinalizeIfLiveRun writes the undo log if the command actually mutated the system.
+func FinalizeIfLiveRun(log *zap.Logger) {
+	if flags.IsLiveRun() {
+		if err := SaveActionLog(); err != nil {
+			log.Warn("❌ Failed to write undo log", zap.Error(err))
+		} else {
+			log.Info("📝 Undo log saved successfully")
+		}
+	}
+}
