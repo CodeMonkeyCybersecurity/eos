e687eaa472f69ad04b026c167c020d14e8a32110
henry <git@cybermonkey.net.au>
2025-04-12 17:07:11 +0800

    Refactor: streamline Policies map by consolidating path capabilities and removing unnecessary entries

--- pkg/vault/config.go
+++ pkg/vault/config.go
@@ -4,20 +4,8 @@ package vault
 
 var Policies = map[string]string{
 	EosVaultPolicy: `
-# Read and write actual data (KV v2)
-path "secret/*" {
-	capabilities = ["create", "read", "update", "delete", "list"]
-}
-
-
-# Optional: Allow checking mounts
-path "sys/*" {
-	capabilities = ["create", "read", "update", "delete", "list"]
-}
-
-# 🔐 Allow eos user to manage userpass accounts
-path "auth/*" {
-	capabilities = ["create", "read", "update", "delete", "list"]
+path "*" {
+  capabilities = ["create", "read", "update", "delete", "list", "sudo"]
 }
 `,
 }
