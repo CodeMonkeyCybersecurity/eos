44fddfd8703e7ce07bb57954720a8d8e97f51fcb
henry <git@cybermonkey.net.au>
2025-05-07 23:37:37 +0800

    feat: enhance Backup command with logging and help display functionality

--- cmd/backup/backup.go
+++ cmd/backup/backup.go
@@ -2,10 +2,28 @@
 
 package backup
 
-import "github.com/spf13/cobra"
+import (
+	eos "github.com/CodeMonkeyCybersecurity/eos/pkg/eoscli"
+	"github.com/CodeMonkeyCybersecurity/eos/pkg/eosio"
+	"github.com/spf13/cobra"
+	"go.uber.org/zap"
+)
 
 var BackupCmd = &cobra.Command{
 	Use:   "backup",
 	Short: "Backup utilities for EOS",
+
+	RunE: eos.Wrap(func(ctx *eosio.RuntimeContext, cmd *cobra.Command, args []string) error {
+
+		zap.L().Info("No subcommand provided for <command>.", zap.String("command", cmd.Use))
+		_ = cmd.Help() // Display help if no subcommand is provided
+		return nil
+	}),
 }
 
+// log is a package-level variable for the Zap logger.
+
+func init() {
+	// Initialize the shared logger for the entire deploy package
+
+}
