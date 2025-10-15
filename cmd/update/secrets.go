dedb8fb02f5bbd757004b37ef715b056aa91267a
henry <git@cybermonkey.net.au>
2025-10-16 06:32:42 +0800

    refactor: remove deprecated vault update command and related imports

--- cmd/update/secrets.go
+++ cmd/update/secrets.go
@@ -2,18 +2,6 @@
 
 package update
 
-import (
-	"os"
-	"os/exec"
-
-	"github.com/CodeMonkeyCybersecurity/eos/pkg/eos_cli"
-	"github.com/CodeMonkeyCybersecurity/eos/pkg/eos_io"
-	"github.com/CodeMonkeyCybersecurity/eos/pkg/platform"
-	"github.com/spf13/cobra"
-	"github.com/uptrace/opentelemetry-go-extra/otelzap"
-	"go.uber.org/zap"
-)
-
 // DEPRECATED: VaultUpdateCmd removed - vault package updates are deprecated
 // Use the new port configuration command: eos update vault --ports FROM -> TO
 // For package updates, use: eos create vault
